9dbf4a4d09447e66014e3f97d91df60bd39b49cc
Simon Jentzsch <simon@jentzschfamily.net>
2016-04-08 08:56:56 +0200

    set permission

--- server/addTx.php
+++ server/addTx.php
@@ -7,9 +7,9 @@ $data = json_decode($json);
 $path = $config->data."/new";
 
 if (!file_exists($path))   mkdir($path, 0777, true);
-
-file_put_contents($path."/".$data->email."-".uniqid().".json",$json);
-
+$p = $path."/".$data->email."-".uniqid().".json"
+file_put_contents($p,$json);
+chmod($p,0775);
 header('Content-Type: application/json');
 echo '{"accepted":true}';
 
