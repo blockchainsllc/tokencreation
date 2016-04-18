16a58cfc849b29d7e7ba0245985752da8fcff2e5
Simon Jentzsch <simon.jentzsch@slock.it>
2016-04-18 09:45:15 +0200

    fixed stats

--- server/tx/updateStats.js
+++ server/tx/updateStats.js
@@ -5,7 +5,12 @@ var code = 'window.daoStats='+JSON.stringify(stats)+"\n" +
            '$(".dao-stat-token-sold").html(window.daoStats.tokens * window.daoStats.units);\n'+
            '$(".dao-stat-days-left").html(window.daoStats.daysLeft);\n'+
            '$(".dao-stat-token-price").html(window.daoStats.price+" ETH");\n'+
-           '$(".dao-stat-total-eth").html(window.daoStats.balance+" ETH");\n';
+           '$(".dao-stat-total-eth").html(window.daoStats.balance+" ETH");\n'+
+           
+           '$(".dao-stat-token-sold-data").attr("data-perc",window.daoStats.tokens * window.daoStats.units);\n'+
+           '$(".dao-stat-days-left-data").attr("data-perc",window.daoStats.daysLeft);\n'+
+           '$(".dao-stat-token-price-data").attr("data-perc",window.daoStats.price);\n'+
+           '$(".dao-stat-total-eth-data").attr("data-perc",window.daoStats.balance);\n';
 
-fs.writeFile(process.argv[2] ||  __dirname+"/stats.js",code);
+fs.writeFile(process.argv[2] ||  __dirname+"/../stats.js",code);
 
