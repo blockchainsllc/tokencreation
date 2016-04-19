c2ffacf4e158235aa9cef51bbbbac01183b39ff0
Simon Jentzsch <simon.jentzsch@slock.it>
2016-04-19 08:59:35 +0200

    renamed all tokensales to tokencreation

---
+++ bower_components/buffer/readme.md
@@ -0,0 +1,6 @@
+'buffer' nodejs core module browserify-ied with `--standalone buffer`. Should support all module systems (commonjs, AMD & `window.buffer`) - check browserify docs.
+
+From [node2web](http://github.com/anodynos/node2web) collection,
+should/will be exposed as 'buffer' to [bower](http://bower.io) for *browser* usage.
+
+browserify version: '3.24.10', build date 'Tue Sep 16 2014 02:22:17 GMT+0300 (EEST)'
---
+++ server/stats.js
@@ -0,0 +1,9 @@
+window.daoStats={"balance":"0.00","price":"1.50","daysLeft":-7,"tokens":"0.00","dao":"0xac8B9227529a6892032088dD6A0CeF2739f1d1b1","units":100,"shapeshift":{"publicKey":"TODO INSERT HERE"},"toc":{"url":"explainer.html","selector":".dao-toc"}}
+$(".dao-stat-token-sold").html(window.daoStats.tokens * window.daoStats.units);
+$(".dao-stat-days-left").html(window.daoStats.daysLeft);
+$(".dao-stat-token-price").html(window.daoStats.price+" ETH");
+$(".dao-stat-total-eth").html(window.daoStats.balance+" ETH");
+$(".dao-stat-token-sold-data").attr("data-perc",window.daoStats.tokens * window.daoStats.units);
+$(".dao-stat-days-left-data").attr("data-perc",window.daoStats.daysLeft);
+$(".dao-stat-token-price-data").attr("data-perc",window.daoStats.price);
+$(".dao-stat-total-eth-data").attr("data-perc",window.daoStats.balance);
