0c1f33c96d00c1a486441586300a8739065114ac
Simon Jentzsch <simon.jentzsch@slock.it>
2016-04-29 06:59:00 +0200

    removed stats.js to avoid conflicts when pulling

--- server/stats.js
+++
@@ -1,9 +0,0 @@
-window.daoStats={"balance":"3000000","price":"1.50","daysLeft":10,"tokens":"39378310","dao":"0xac8B9227529a6892032088dD6A0CeF2739f1d1b1","units":100,"shapeshift":{"publicKey":"TODO INSERT HERE"},"toc":{"url":"explainer.html","selector":".dao-toc"}}
-$(".dao-stat-token-sold").html(window.daoStats.tokens * window.daoStats.units);
-$(".dao-stat-days-left").html(window.daoStats.daysLeft);
-$(".dao-stat-token-price").html(window.daoStats.price+" ETH");
-$(".dao-stat-total-eth").html(window.daoStats.balance+" ETH");
-$(".dao-stat-token-sold-data").attr("data-perc",window.daoStats.tokens * window.daoStats.units);
-$(".dao-stat-days-left-data").attr("data-perc",window.daoStats.daysLeft);
-$(".dao-stat-token-price-data").attr("data-perc",window.daoStats.price);
-$(".dao-stat-total-eth-data").attr("data-perc",window.daoStats.balance);
